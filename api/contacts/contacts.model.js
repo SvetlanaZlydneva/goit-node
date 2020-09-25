3a29c7fd27d8c42671565b1fb460272d38e32d84
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2020-09-25 11:49:10 +0300

    hw-03

---
+++ api/contacts/contacts.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const contactSchema = new Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  phone: { type: String, require: true },
+});
+
+mongoose.set("useFindAndModify", false);
+
+//contatcts
+module.exports = mongoose.model("Contact", contactSchema);
